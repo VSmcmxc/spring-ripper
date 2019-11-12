4dbd0cef0ab36bb2be313ec0510cb71b74cbd74c
Vadim Salimov <vadim.mcmxc@gmail.com>
2019-11-12 16:22:37 +0300

    Part 7

---
+++ src/main/java/com/epam/screensaver/ColorFrame.java
@@ -0,0 +1,30 @@
+package com.epam.screensaver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+@Service
+public class ColorFrame extends JFrame {
+
+    @Autowired
+    private Color color;
+
+    public ColorFrame() {
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace() {
+        Random random = new Random();
+        setLocation(random.nextInt(1200), random.nextInt(900));
+        getContentPane().setBackground(color);
+        repaint();
+
+    }
+}
